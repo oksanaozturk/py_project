af082cfab8aaff30f259d3bf7a61d666dcff64d8
Oksana Ozturk <oksanaozturk@gmail.com>
2023-10-11 21:32:19 +0300

    add function subtracting_numbers

--- funcs.py
+++ funcs.py
@@ -1,3 +1,8 @@
 def adding_numbers(a: int, b: int) -> int:
     """Функция сложения двух чисел"""
     return a + b
+
+
+def subtracting_numbers(a: int, b: int) -> int:
+    """Функцмя вычитания двух чисел"""
+    return a-b
